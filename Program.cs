d85d0308916499ebc211e67c31c487322b8f7c07
Pavel Peredrij <peredrij.pavel19@gmail.com>
2023-07-31 21:24:19 +0300

    added all files

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Task 2. Напишите программу, которая на вход принимает два числа
+// и выдаёт какое число большее, а какое меньшее.
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2) Console.WriteLine(num1);
+else Console.WriteLine(num2);
+*/
+// Task 4. Напишите программу, которая принимает на вход три числа и выдаёт
+// максимальное из этих чисел.
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else if(num2 > num3)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine(num3);
+}
